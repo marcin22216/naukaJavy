7e8c7a3b357ee802458849ef166b28d788481ae6
Pawel Gondek <Pawel.Gondek@sabre.com>
2018-09-17 22:08:45 +0200

    Example of Main

---
+++ src/main/java/firstLesson/MainToy.java
@@ -0,0 +1,13 @@
+package firstLesson;
+
+public class MainToy {
+
+    public static void main(String[] args) {
+
+        Toy toy = new Toy();
+
+        toy.setPrice(1);
+        System.out.println(toy.getPrice());
+    }
+}
+
