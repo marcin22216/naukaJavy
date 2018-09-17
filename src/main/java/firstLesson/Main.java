d030e67a4bc4095a64948f990acaf90b2a980f99
Pawel Gondek <Pawel.Gondek@sabre.com>
2018-09-17 20:49:24 +0200

    clean up

--- src/main/java/firstLesson/Main.java
+++ src/main/java/firstLesson/Main.java
@@ -7,15 +7,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        Human h1 = new Human(34);
-//        Human h2 = new Human(14);
-//
-//
-//        int age = h2.getAge();
-//
-//        //sout
-//        System.out.println(age);
-
         PiecZlotych dupa = new PiecZlotych();
 
 
