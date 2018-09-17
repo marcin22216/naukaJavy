ad19f5434921e62ebe31b19c78b73c02fd565d1b
Pawel Gondek <Pawel.Gondek@sabre.com>
2018-09-17 21:48:07 +0200

    getters and setter

---
+++ src/main/java/firstLesson/BankAccount.java
@@ -0,0 +1,25 @@
+package firstLesson;
+
+public class BankAccount {
+
+    private double balance;
+
+
+    //settery
+
+    public void setBalance(double szufladkaDlaTymczasowegoBalance)
+    {
+        this.balance = szufladkaDlaTymczasowegoBalance;
+    }
+
+
+    //getter
+    public double getBalance()
+    {
+        return this.balance;
+    }
+
+
+
+
+}
--- src/main/java/firstLesson/Main.java
+++ src/main/java/firstLesson/Main.java
@@ -2,10 +2,22 @@ package firstLesson;
 
 public class Main {
 
+    //psvm
     public static void main(String[] args) {
 
+        new BankAccount();
+        BankAccount dupa = new BankAccount();
+        BankAccount bankAccount = new BankAccount();
 
-        PiecZlotych dupa = new PiecZlotych();
+        dupa.setBalance(1500);
+
+        double balance = dupa.getBalance();
+
+        //sout
+//        System.out.println(balance);
+
+
+        System.out.println(bankAccount.getBalance());
 
 
     }
