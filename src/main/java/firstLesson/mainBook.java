5786ad0167e3745143f8d172d95682e28ec5590e
marcin22216 <marcin22216@gmail.com>
2018-09-19 22:04:19 +0200

    new washer

--- src/main/java/firstLesson/mainBook.java
+++ src/main/java/firstLesson/mainBook.java
@@ -7,7 +7,7 @@ public class MainBook {
         Book stephenKing = new Book();
 
         stephenKing.setPrice(19.99);
-        System.out.println(stephenKing.getPrice());
+        System.out.println(Book.getPrice());
 
     }
 
